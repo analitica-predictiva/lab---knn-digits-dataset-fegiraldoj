cb70ad5fb7abac4871103cb32bf3aa4263d5bcad
fegiraldoj <101747510+fegiraldoj@users.noreply.github.com>
2022-12-01 20:56:37 -0500

    pregunta 01

--- preguntas.py
+++ preguntas.py
@@ -26,7 +26,7 @@ def pregunta_01():
     print(digits.data.shape)
 
     # Imprima las dimensiones del vector de salida
-    print(digits.images.shape)
+    print(digits.target.shape)
 
 
 def pregunta_02():
